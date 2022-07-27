401c58b5c201b93f2ed39b304dd26b7b940fcf99
eric2918 <11042876+eric2918@user.noreply.gitee.com>
2022-07-27 16:29:33 +0800

    init

---
+++ leaf.go
@@ -0,0 +1,47 @@
+package leaf
+
+import (
+	"os"
+	"os/signal"
+
+	"github.com/eric2918/leaf/cluster"
+	"github.com/eric2918/leaf/conf"
+	"github.com/eric2918/leaf/console"
+	"github.com/eric2918/leaf/log"
+	"github.com/eric2918/leaf/module"
+)
+
+func Run(mods ...module.Module) {
+	// logger
+	if conf.LogLevel != "" {
+		logger, err := log.New(conf.LogLevel, conf.LogPath, conf.LogFlag)
+		if err != nil {
+			panic(err)
+		}
+		log.Export(logger)
+		defer logger.Close()
+	}
+
+	log.Release("Leaf %v starting up", version)
+
+	// module
+	for i := 0; i < len(mods); i++ {
+		module.Register(mods[i])
+	}
+	module.Init()
+
+	// cluster
+	cluster.Init()
+
+	// console
+	console.Init()
+
+	// close
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill)
+	sig := <-c
+	log.Release("Leaf closing down (signal: %v)", sig)
+	console.Destroy()
+	cluster.Destroy()
+	module.Destroy()
+}
